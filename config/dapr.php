d70bdf563fe2c5fd381f1a51d7ec9aa7099a7152
Mohammed Azman <azmman2012@gmail.com>
2025-12-04 03:36:57 +0300

    enahanced dapr invoker and add route dapr invoker list

--- config/dapr.php
+++ config/dapr.php
@@ -36,4 +36,22 @@ return [
         // a callable class you can override for custom checks (optional)
         'checker'  => env('DAPR_HEALTH_CHECKER', null), /**  \AlazziAz\LaravelDapr\Support\HealthCheckerInterface::class  */
     ],
+    'invocation' => [
+        'prefix' => 'dapr/invoke',
+
+        'auto_register' => false,
+
+        'middleware' => [
+            // \App\Http\Middleware\Authenticate::class,
+        ],
+
+        'verify_signature' => false,
+        'signature_header' => 'x-dapr-signature',
+        'signature_secret' => env('DAPR_INVOKE_SECRET'),
+
+        'map' => [
+            // 'service.method' => App\Http\Controllers\InvokeTargetController::class,
+            // 'orders.create' => [App\Http\Controllers\OrderController::class, 'createViaInvoke'],
+        ],
+    ]
 ];
